5f6e41b429101b851aed9d70f5f431556c823e02
unknown <joshuaheathcote1987@gmail.com>
2024-09-20 23:34:11 +0300

    first commit

---
+++ app/(tabs)/mushrooms.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const mushrooms = () => {
+    return (
+        <View>
+            <Text>mushrooms</Text>
+        </View>
+    )
+}
+
+export default mushrooms
+
+const styles = StyleSheet.create({})
